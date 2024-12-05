bb4fa35c87755b6f348ed09deff782e4d0db46b8
Louve_Gasqui <lgasqui@student.42.fr>
2024-12-05 14:59:26 +0100

    FIX : Doublons de \n

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lgasqui <lgasqui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:24:19 by lgasqui           #+#    #+#             */
-/*   Updated: 2024/11/05 09:26:36 by lgasqui          ###   ########.fr       */
+/*   Updated: 2024/12/05 14:49:05 by lgasqui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,8 @@ int	main(int argc, char **argv)
 			if(argv[i + 1])
 				std::cout << " ";
 		}
-		
+		std::cout << std::endl;	
 	}
 	
-	std::cout << std::endl;
 	return 0;
 } 
