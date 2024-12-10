7eda76edad85aa70322a07e6a4b2e9dbff6e27bc
Louve_Gasqui <lgasqui@student.42.fr>
2024-12-10 10:46:49 +0100

    FIX : protected against ctrl D

--- ex01/Contacts.cpp
+++ ex01/Contacts.cpp
@@ -6,7 +6,7 @@
 /*   By: lgasqui <lgasqui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 10:01:10 by lgasqui           #+#    #+#             */
-/*   Updated: 2024/12/05 15:14:54 by lgasqui          ###   ########.fr       */
+/*   Updated: 2024/12/10 10:40:38 by lgasqui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,38 @@ void Contacts::first_name_input()
 	{
 		std::cout << "First Name :";
 		std::getline (std::cin, FirstName);
+        if(std::cin.eof())
+        {
+            break;
+        }
         bool valid = true;
+        bool hasLetter = false;
         for (std::string::size_type i = 0; i < FirstName.length(); ++i)
         {
+            if (std::isdigit(FirstName[i])) // Vérifie si un chiffre est présent
+            {
+                valid = false;
+                break;
+            }
+            if (std::isalpha(FirstName[i])) // Vérifie si une lettre est présente
+            {
+                hasLetter = true;
+            }
             if (!std::isprint(FirstName[i]))
             {
                 valid = false;
                 break;
             }
         }
-
-        if (!valid)
+        if (FirstName.empty() || !hasLetter || !valid)
         {
-            std::cout << "Error: Firstname contains non-printable characters.\n";
-            FirstName.clear(); // Réinitialise NickName pour forcer un nouvel essai
+            std::cout << "Error: First name must contain at least one letter, "
+                         "no numbers, and no invalid characters.\n";
+            FirstName.clear();
+        }
+        else
+        {
+            break;
         }
 	}
 }
@@ -52,20 +70,38 @@ void Contacts::last_name_input()
 	{
 		std::cout << "Last Name :";
 		std::getline (std::cin, LastName);
+        if(std::cin.eof())
+        {
+            break;
+        }
         bool valid = true;
+        bool hasLetter = false;
         for (std::string::size_type i = 0; i < LastName.length(); ++i)
         {
+            if (std::isdigit(LastName[i])) // Vérifie si un chiffre est présent
+            {
+                valid = false;
+                break;
+            }
+            if (std::isalpha(LastName[i])) // Vérifie si une lettre est présente
+            {
+                hasLetter = true;
+            }
             if (!std::isprint(LastName[i]))
             {
                 valid = false;
                 break;
             }
         }
-
-        if (!valid)
+        if (LastName.empty() || !hasLetter || !valid)
         {
-            std::cout << "Error: Lastname contains non-printable characters.\n";
-            LastName.clear(); // Réinitialise NickName pour forcer un nouvel essai
+            std::cout << "Error: Last name must contain at least one letter, "
+                         "no numbers, and no invalid characters.\n";
+            LastName.clear();
+        }
+        else
+        {
+            break;
         }
 	}
 }
@@ -75,12 +111,36 @@ void Contacts::nick_name_input()
 	{
 		std::cout << "Nick Name :";
 		std::getline (std::cin, NickName);
+        if(std::cin.eof())
+        {
+            break;
+        }
         bool valid = true;
+        bool hasLetter = false;
         for (std::string::size_type i = 0; i < NickName.length(); ++i)
         {
+            if (std::isdigit(NickName[i])) // Vérifie si un chiffre est présent
+            {
+                valid = false;
+                break;
+            }
+            if (std::isalpha(NickName[i])) // Vérifie si une lettre est présente
+            {
+                hasLetter = true;
+            }
             if (!std::isprint(NickName[i]))
             {
                 valid = false;
+                break;
+            }
+            if (NickName.empty() || !hasLetter || !valid)
+            {
+                std::cout << "Error: Nick name must contain at least one letter, "
+                         "no numbers, and no invalid characters.\n";
+                NickName.clear();
+            }
+            else
+            {
                 break;
             }
         }
@@ -95,34 +155,38 @@ void Contacts::nick_name_input()
 
 void Contacts::phone_number_input()
 {
-    //while (true)
-    //{
+    while (true)
+    {
         std::cout << "Phone Number : ";
         std::getline(std::cin, PhoneNumber);
+        if(std::cin.eof())
+        {
+            break;
+        }
 
-        // bool isNumeric = true;
+        bool isNumeric = true;
 		
-        // for (std::string::size_type i = 0; i < PhoneNumber.length(); ++i)
-        // {
-        //     if (!std::isdigit(PhoneNumber[i]))
-        //     {
-        //         isNumeric = false;
-        //         break;
-        //     }
-        // }
-        // if (!isNumeric)
-        // {
-        //     std::cout << "Error: The input must contain only digits.\n";
-        //     continue;
-        // }
-        // if (PhoneNumber.length() < 10 || PhoneNumber.length() > 10)
-        // {
-        //     std::cout << "Error, invalid phone number format 10 digits are required.\n";
-        //     continue;
-        // }
+        for (std::string::size_type i = 0; i < PhoneNumber.length(); ++i)
+        {
+            if (!std::isdigit(PhoneNumber[i]))
+            {
+                isNumeric = false;
+                break;
+            }
+        }
+        if (!isNumeric)
+        {
+            std::cout << "Error: The input must contain only digits.\n";
+            continue;
+        }
+        if (PhoneNumber.length() < 10 || PhoneNumber.length() > 10)
+        {
+            std::cout << "Error, invalid phone number format 10 digits are required.\n";
+            continue;
+        }
 
-        // break;
-   // }
+        break;
+    }
 }
 
 void Contacts::darkest_secret_input()
@@ -131,20 +195,38 @@ void Contacts::darkest_secret_input()
 	{
 		std::cout << "Darkest Secret :";
 		std::getline (std::cin, DarkestSecret);
+        if(std::cin.eof())
+        {
+            break;
+        }
         bool valid = true;
+        bool hasLetter = false;
         for (std::string::size_type i = 0; i < DarkestSecret.length(); ++i)
         {
+            if (std::isdigit(DarkestSecret[i])) // Vérifie si un chiffre est présent
+            {
+                valid = false;
+                break;
+            }
+            if (std::isalpha(DarkestSecret[i])) // Vérifie si une lettre est présente
+            {
+                hasLetter = true;
+            }
             if (!std::isprint(DarkestSecret[i]))
             {
                 valid = false;
                 break;
             }
         }
-
-        if (!valid)
+        if (DarkestSecret.empty() || !hasLetter || !valid)
         {
-            std::cout << "Error: Darkest Secret contains non-printable characters.\n";
-            DarkestSecret.clear(); // Réinitialise NickName pour forcer un nouvel essai
+            std::cout << "Error: Darkest Secret must contain at least one letter, "
+                         "no numbers, and no invalid characters.\n";
+            DarkestSecret.clear();
+        }
+        else
+        {
+            break;
         }
 	}
 }
